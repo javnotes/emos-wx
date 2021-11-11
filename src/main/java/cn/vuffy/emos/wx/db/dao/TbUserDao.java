e0cd2c4be69bb1718216b77e4b46ecbabaa6df19
vuffy <monkeyd.vuffy@gmail.com>
2021-11-11 13:10:21 +0800

    coding admin register

--- src/main/java/cn/vuffy/emos/wx/db/dao/TbUserDao.java
+++ src/main/java/cn/vuffy/emos/wx/db/dao/TbUserDao.java
@@ -1,7 +1,16 @@
 package cn.vuffy.emos.wx.db.dao;
 
+import cn.vuffy.emos.wx.db.pojo.TbUser;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.HashMap;
 
 @Mapper
 public interface TbUserDao {
+    public boolean haveRootUser();
+
+    public Integer searchIdByOpenId(String openId);
+
+    public int insert(HashMap param);
+
 }
--- src/main/java/cn/vuffy/emos/wx/db/pojo/TbUser.java
+++ src/main/java/cn/vuffy/emos/wx/db/pojo/TbUser.java
@@ -1,38 +1,211 @@
 package cn.vuffy.emos.wx.db.pojo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.io.Serializable;
-import lombok.Data;
+import java.util.Date;
 
 /**
- * sys_config
+ * tb_user
  * @author 
  */
-@Data
+@ApiModel(value="cn.vuffy.emos.wx.db.pojo.TbUser用户表")
 public class TbUser implements Serializable {
     /**
      * 主键
      */
+    @ApiModelProperty(value="主键")
     private Integer id;
 
     /**
-     * 参数名
+     * 长期授权字符串
      */
-    private String paramKey;
+    @ApiModelProperty(value="长期授权字符串")
+    private String openId;
 
     /**
-     * 参数值
+     * 昵称
      */
-    private String paramValue;
+    @ApiModelProperty(value="昵称")
+    private String nickname;
+
+    /**
+     * 头像网址
+     */
+    @ApiModelProperty(value="头像网址")
+    private String photo;
+
+    /**
+     * 姓名
+     */
+    @ApiModelProperty(value="姓名")
+    private String name;
+
+    /**
+     * 性别
+     */
+    @ApiModelProperty(value="性别")
+    private Object sex;
+
+    /**
+     * 手机号码
+     */
+    @ApiModelProperty(value="手机号码")
+    private String tel;
+
+    /**
+     * 邮箱
+     */
+    @ApiModelProperty(value="邮箱")
+    private String email;
+
+    /**
+     * 入职日期
+     */
+    @ApiModelProperty(value="入职日期")
+    private Date hiredate;
+
+    /**
+     * 角色
+     */
+    @ApiModelProperty(value="角色")
+    private Object role;
+
+    /**
+     * 是否是超级管理员
+     */
+    @ApiModelProperty(value="是否是超级管理员")
+    private Boolean root;
+
+    /**
+     * 部门编号
+     */
+    @ApiModelProperty(value="部门编号")
+    private Integer deptId;
 
     /**
      * 状态
      */
+    @ApiModelProperty(value="状态")
     private Byte status;
 
     /**
-     * 备注
+     * 创建时间
      */
-    private String remark;
+    @ApiModelProperty(value="创建时间")
+    private Date createTime;
 
     private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getSex() {
+        return sex;
+    }
+
+    public void setSex(Object sex) {
+        this.sex = sex;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(Date hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public Object getRole() {
+        return role;
+    }
+
+    public void setRole(Object role) {
+        this.role = role;
+    }
+
+    public Boolean getRoot() {
+        return root;
+    }
+
+    public void setRoot(Boolean root) {
+        this.root = root;
+    }
+
+    public Integer getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Integer deptId) {
+        this.deptId = deptId;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
 }
