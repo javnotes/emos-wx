1293a0ddc28e9807a46ae0b5d6ff0a326d017b52
vuffy <monkeyd.vuffy@gmail.com>
2021-11-23 22:23:04 +0800

    checkin dao

--- src/main/java/cn/vuffy/emos/wx/db/dao/TbCheckinDao.java
+++ src/main/java/cn/vuffy/emos/wx/db/dao/TbCheckinDao.java
@@ -2,6 +2,9 @@ package cn.vuffy.emos.wx.db.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.HashMap;
+
 @Mapper
 public interface TbCheckinDao {
+    public Integer haveCheckin(HashMap param);
 }
